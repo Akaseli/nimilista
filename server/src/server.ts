2087eee50e26dff1e4cdcd56c27d5bd684f97f55
FinFuz <60463103+Akaseli@users.noreply.github.com>
2022-02-15 20:28:58 +0200

    Serverin alku

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import * as dotenv from "dotenv";
 const app = express()
 const port = 4000;
 
+dotenv.config({ path: __dirname+'/.env' });
+
 app.get("/", (req, res) => {
-    res.send({online: true})
+    res.send("Value " + process.env.TEST)
 });
 
 app.listen(port, () => {
