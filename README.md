93bb9bab88791cab6d5c731a4b194ca69deeb062
FinFuz <60463103+Akaseli@users.noreply.github.com>
2022-02-15 19:29:42 +0200

    serverin alku

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# nimikirja
+# nimilista
  
---
+++ server/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 4000;
+app.get('/', function (req, res) {
+    res.send({ online: true });
+});
+app.get('/people', function (req, res) {
+    res.send({ online: true });
+});
+app.listen(port, function () {
+    return console.log("Listening on ".concat(port));
+});
